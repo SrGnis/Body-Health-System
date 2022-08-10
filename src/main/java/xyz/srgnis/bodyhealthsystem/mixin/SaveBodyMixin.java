fda262b1d4198be6b87e9a4ee3a7028ce25e443a
SrGnis <gnisms97@gmail.com>
2022-08-10 14:03:53 +0200

    Body data sync on player join and "syncBody" method

--- src/main/java/xyz/srgnis/bodyhealthsystem/mixin/SaveBodyMixin.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/mixin/SaveBodyMixin.java
@@ -1,12 +1,17 @@
 package xyz.srgnis.bodyhealthsystem.mixin;
 
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.srgnis.bodyhealthsystem.BHSMain;
+import xyz.srgnis.bodyhealthsystem.body.BodyPart;
 import xyz.srgnis.bodyhealthsystem.body.PlayerBodyProvider;
 
 @Mixin(PlayerEntity.class)
@@ -18,6 +23,8 @@ public class SaveBodyMixin {
 
     @Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
     public void deserializeBodyParts(NbtCompound tag, CallbackInfo ci) {
-        ((PlayerBodyProvider)this).getBody().readFromNbt(tag);
+        PlayerBodyProvider pe = (PlayerBodyProvider)this;
+        pe.getBody().readFromNbt(tag);
+        BHSMain.LOGGER.warn("BODY READ");
     }
 }
