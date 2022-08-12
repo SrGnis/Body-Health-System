4b58d2ac5e442cf3a0a26a75b00f15ce19fd839e
SrGnis <gnisms97@gmail.com>
2022-08-12 14:59:34 +0200

    Dev Commands and heal and damage methods

--- src/main/java/xyz/srgnis/bodyhealthsystem/body/player/PlayerBody.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/body/player/PlayerBody.java
@@ -1,12 +1,16 @@
 package xyz.srgnis.bodyhealthsystem.body.player;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import xyz.srgnis.bodyhealthsystem.body.Body;
 import xyz.srgnis.bodyhealthsystem.body.player.parts.*;
 
+import javax.swing.text.html.parser.Entity;
+
 public class PlayerBody extends Body {
 
     public PlayerBody(PlayerEntity player) {
+        this.entity = player;
         this.addPart(PlayerBodyParts.HEAD, new HeadBodyPart(player));
         this.addPart(PlayerBodyParts.TORSO, new TorsoBodyPart(player));
         this.addPart(PlayerBodyParts.LEFT_ARM, new ArmBodyPart("left",player));
