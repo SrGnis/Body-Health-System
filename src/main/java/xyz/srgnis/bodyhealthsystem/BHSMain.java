d6455320e5979d83a562a65e0d83e31c23503773
SrGnis <gnisms97@gmail.com>
2022-10-06 19:29:54 +0200

    First item added

--- src/main/java/xyz/srgnis/bodyhealthsystem/BHSMain.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/BHSMain.java
@@ -1,12 +1,20 @@
 package xyz.srgnis.bodyhealthsystem;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.srgnis.bodyhealthsystem.client.hud.BHSHud;
 import xyz.srgnis.bodyhealthsystem.command.DevCommands;
+import xyz.srgnis.bodyhealthsystem.items.BandageItem;
 import xyz.srgnis.bodyhealthsystem.network.ClientNetworking;
 import xyz.srgnis.bodyhealthsystem.network.ServerNetworking;
 
@@ -19,13 +27,25 @@ public class BHSMain implements ModInitializer {
 	public  static Identifier MOD_IDENTIFIER = new Identifier(BHSMain.MOD_ID);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final ItemGroup BHS_GROUP = FabricItemGroupBuilder.create(
+			new Identifier("bodyhealthsystem", "general"))
+			.icon(() -> new ItemStack(Items.BOWL))
+			.build();
+
+	public static final Item BANDAGE_ITEM = new BandageItem(new FabricItemSettings().group(BHS_GROUP));
+
 	@Override
 	public void onInitialize() {
 		HudRenderCallback.EVENT.register(new BHSHud());
 		ClientNetworking.initialize();
 		ServerNetworking.initialize();
 		DevCommands.initialize();
+		registerItems();
 
+	}
+
+	public void registerItems(){
+		Registry.register(Registry.ITEM, new Identifier("bodyhealthsystem", "bandage"), BANDAGE_ITEM);
 	}
 
 }
---
+++ src/main/java/xyz/srgnis/bodyhealthsystem/items/BandageItem.java
@@ -0,0 +1,27 @@
+package xyz.srgnis.bodyhealthsystem.items;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class BandageItem extends Item {
+    public BandageItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        if(playerEntity.getHealth() < playerEntity.getMaxHealth()){
+            ItemStack itemStack = playerEntity.getStackInHand(hand);
+            playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
+            playerEntity.heal(2);
+            itemStack.decrement(1);
+            return TypedActionResult.consume(itemStack);
+        }
+        return TypedActionResult.fail(playerEntity.getStackInHand(hand));
+    }
+}
