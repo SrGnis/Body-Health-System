252054f0740a716791e69b78eff483dbe8f7c9a7
SrGnis <gnisms97@gmail.com>
2022-10-17 19:24:41 +0200

    Basic GUI test

--- src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
@@ -2,28 +2,41 @@ package xyz.srgnis.bodyhealthsystem.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
 import net.minecraft.util.Identifier;
+import xyz.srgnis.bodyhealthsystem.body.Body;
+import xyz.srgnis.bodyhealthsystem.body.player.BodyProvider;
+import xyz.srgnis.bodyhealthsystem.body.player.PlayerBodyParts;
+
+import java.util.List;
+import java.util.Objects;
+
+import static net.minecraft.client.gui.DrawableHelper.fill;
 
 public class GenericScreen extends HandledScreen<ScreenHandler> {
-    private static final Identifier TEXTURE = new Identifier("textures/gui/container/generic_54.png");
+    public static final Text HEAD = Text.literal("HEAD");
+    private Body body;
 
     public GenericScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
+        this.body = ((BodyProvider)inventory.player).getBody();
     }
 
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE);
         int i = (this.width - this.backgroundWidth) / 2;
         int j = (this.height - this.backgroundHeight) / 2;
-        this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, 3 * 18 + 17);
-        this.drawTexture(matrices, i, j + 3 * 18 + 17, 0, 126, this.backgroundWidth, 96);
+        fill(matrices, i, j, i+this.backgroundWidth, j+this.backgroundHeight, 0xffffffff);
+        this.addDrawableChild(new ButtonWidget(i, j, 50, 20, HEAD, button -> {
+            body.healPart(100,body.getPart(PlayerBodyParts.HEAD));
+        }));
     }
 }
