882e6c61bd80a10c5e68057e54e1df9c612f7076
SrGnis <gnisms97@gmail.com>
2022-10-17 14:39:57 +0200

    basic

--- src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
@@ -10,6 +10,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class GenericScreen extends HandledScreen<ScreenHandler> {
+    private static final Identifier TEXTURE = new Identifier("textures/gui/container/generic_54.png");
 
     public GenericScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -19,8 +20,10 @@ public class GenericScreen extends HandledScreen<ScreenHandler> {
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        int i = this.x;
-        int j = this.y;
-        this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int i = (this.width - this.backgroundWidth) / 2;
+        int j = (this.height - this.backgroundHeight) / 2;
+        this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, 3 * 18 + 17);
+        this.drawTexture(matrices, i, j + 3 * 18 + 17, 0, 126, this.backgroundWidth, 96);
     }
 }
