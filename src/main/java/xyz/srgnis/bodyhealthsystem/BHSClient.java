ca2177f15b86e65420970f3b4b8672fddf8f39b8
SrGnis <gnisms97@gmail.com>
2022-10-17 13:03:34 +0200

    copy code from fabric api

--- src/main/java/xyz/srgnis/bodyhealthsystem/BHSClient.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/BHSClient.java
@@ -2,6 +2,8 @@ package xyz.srgnis.bodyhealthsystem;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.screen.ingame.Generic3x3ContainerScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import xyz.srgnis.bodyhealthsystem.client.hud.BHSHud;
 import xyz.srgnis.bodyhealthsystem.network.ClientNetworking;
 
@@ -10,5 +12,6 @@ public class BHSClient implements ClientModInitializer {
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register(new BHSHud());
         ClientNetworking.initialize();
+        HandledScreens.register(BHSMain.BAG_SCREEN_HANDLER, Generic3x3ContainerScreen::new);
     }
 }
--- src/main/java/xyz/srgnis/bodyhealthsystem/BHSMain.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/BHSMain.java
@@ -3,14 +3,19 @@ package xyz.srgnis.bodyhealthsystem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.srgnis.bodyhealthsystem.client.BagScreenHandler;
 import xyz.srgnis.bodyhealthsystem.client.hud.BHSHud;
 import xyz.srgnis.bodyhealthsystem.command.DevCommands;
 import xyz.srgnis.bodyhealthsystem.config.Config;
+import xyz.srgnis.bodyhealthsystem.items.BagItem;
 import xyz.srgnis.bodyhealthsystem.network.ClientNetworking;
 import xyz.srgnis.bodyhealthsystem.network.ServerNetworking;
 import xyz.srgnis.bodyhealthsystem.registry.ModItems;
@@ -24,6 +29,9 @@ public class BHSMain implements ModInitializer {
 			new Identifier(MOD_ID, "general"))
 			.icon(() -> new ItemStack(ModItems.PLASTER_ITEM))
 			.build();
+
+	public static final ScreenHandlerType<BagScreenHandler> BAG_SCREEN_HANDLER = new ScreenHandlerType<>(BagScreenHandler::new);
+	public static final Item BAG = new BagItem(new Item.Settings().group(ItemGroup.TOOLS).maxCount(1));
 	@Override
 	public void onInitialize() {
 		ServerNetworking.initialize();
@@ -31,5 +39,7 @@ public class BHSMain implements ModInitializer {
 		ModItems.registerItems();
 		ModStatusEffects.registerStatusEffects();
 		Config.init(MOD_ID, Config.class);
+		Registry.register(Registry.SCREEN_HANDLER, new Identifier(MOD_ID,"bag"), BAG_SCREEN_HANDLER);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID,"bag"), BAG);
 	}
 }
