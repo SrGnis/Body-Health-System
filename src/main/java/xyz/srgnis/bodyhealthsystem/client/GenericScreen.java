6acbe30edbb3d0d3dae2deef4d86a6e35b12198f
SrGnis <gnisms97@gmail.com>
2022-10-18 15:00:54 +0200

    Very basic screen

--- src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/client/GenericScreen.java
@@ -1,29 +1,30 @@
 package xyz.srgnis.bodyhealthsystem.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.text.OrderedText;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
-import net.minecraft.util.Identifier;
 import xyz.srgnis.bodyhealthsystem.body.Body;
 import xyz.srgnis.bodyhealthsystem.body.player.BodyProvider;
 import xyz.srgnis.bodyhealthsystem.body.player.PlayerBodyParts;
 
-import java.util.List;
-import java.util.Objects;
-
-import static net.minecraft.client.gui.DrawableHelper.fill;
-
 public class GenericScreen extends HandledScreen<ScreenHandler> {
     public static final Text HEAD = Text.literal("HEAD");
+    public static final Text LEFT_ARM = Text.literal("LEFT ARM");
+    public static final Text RIGHT_ARM = Text.literal("RIGHT ARM");
+    public static final Text TORSO = Text.literal("TORSO");
+    public static final Text LEFT_LEG = Text.literal("LEFT LEG");
+    public static final Text RIGHT_LEG = Text.literal("RIGHT LEG");
+    public static final Text LEFT_FOOT = Text.literal("LEFT FOOT");
+    public static final Text RIGHT_FOOT = Text.literal("RIGHT FOOT");
+
     private Body body;
+    private int buttonWidth = 75;
+    private int buttonHeight = 20;
+    private int buttonMargin = 2;
+    private int total = (buttonHeight+buttonMargin)*5;
 
     public GenericScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -32,11 +33,43 @@ public class GenericScreen extends HandledScreen<ScreenHandler> {
 
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        int i = (this.width - this.backgroundWidth) / 2;
-        int j = (this.height - this.backgroundHeight) / 2;
-        fill(matrices, i, j, i+this.backgroundWidth, j+this.backgroundHeight, 0xffffffff);
-        this.addDrawableChild(new ButtonWidget(i, j, 50, 20, HEAD, button -> {
-            body.healPart(100,body.getPart(PlayerBodyParts.HEAD));
+        int row = 0;
+        int startX = (this.width - this.backgroundWidth) / 2;
+        int startY = (this.height - this.backgroundHeight) / 2;
+        int buttonStartY = startY+((this.backgroundHeight - total) / 2);
+        int centerX = ((this.backgroundWidth - buttonWidth) / 2) + startX;
+        int centerLeftX = ((this.backgroundWidth - buttonWidth * 2) / 2) + startX;
+
+        fill(matrices, startX, startY, startX+this.backgroundWidth, startY+this.backgroundHeight, 0xffffffff);
+
+        this.addDrawableChild(new ButtonWidget(centerX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, HEAD, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.HEAD));
+        }));
+        row++;
+        this.addDrawableChild(new ButtonWidget(centerLeftX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, LEFT_ARM, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.LEFT_ARM));
+        }));
+
+        this.addDrawableChild(new ButtonWidget(centerLeftX+buttonMargin+buttonWidth, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, RIGHT_ARM, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.RIGHT_ARM));
+        }));
+        row++;
+        this.addDrawableChild(new ButtonWidget(centerX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, RIGHT_ARM, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.TORSO));
+        }));
+        row++;
+        this.addDrawableChild(new ButtonWidget(centerLeftX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, LEFT_LEG, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.LEFT_LEG));
+        }));
+        this.addDrawableChild(new ButtonWidget(centerLeftX+buttonMargin+buttonWidth, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, RIGHT_LEG, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.RIGHT_LEG));
+        }));
+        row++;
+        this.addDrawableChild(new ButtonWidget(centerLeftX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, LEFT_FOOT, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.LEFT_FOOT));
+        }));
+        this.addDrawableChild(new ButtonWidget(centerLeftX+buttonMargin+buttonWidth, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, RIGHT_FOOT, button -> {
+            body.healPart(4,body.getPart(PlayerBodyParts.RIGHT_FOOT));
         }));
     }
 }
