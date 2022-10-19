4811f3c88f182861748baa5de7b49d4dd05de2eb
SrGnis <gnisms97@gmail.com>
2022-10-19 15:01:39 +0200

    code revision

--- src/main/java/xyz/srgnis/bodyhealthsystem/client/HealScreen.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/client/HealScreen.java
@@ -7,7 +7,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import xyz.srgnis.bodyhealthsystem.body.Body;
+import xyz.srgnis.bodyhealthsystem.body.BodyPart;
 import xyz.srgnis.bodyhealthsystem.body.player.BodyProvider;
 import xyz.srgnis.bodyhealthsystem.body.player.PlayerBodyParts;
 import xyz.srgnis.bodyhealthsystem.network.ClientNetworking;
@@ -29,10 +31,30 @@ public class HealScreen extends HandledScreen<ScreenHandler> {
     private final int buttonMargin = 2;
     private final int total = (buttonHeight+buttonMargin)*5;
 
+    private final int heightAndMargin = buttonHeight+buttonMargin;
+
+    BodyPartHealButton headButton;
+    BodyPartHealButton leftArmButton;
+    BodyPartHealButton rightArmButton;
+    BodyPartHealButton torsoButton;
+    BodyPartHealButton leftLegButton;
+    BodyPartHealButton rightLegButton;
+    BodyPartHealButton leftFootButton;
+    BodyPartHealButton rightFootButton;
+
     public HealScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
         this.body = ((BodyProvider)inventory.player).getBody();
         this.player = inventory.player;
+
+        headButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.HEAD),0, 0, buttonWidth, buttonHeight, HEAD);
+        leftArmButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.LEFT_ARM),0, 0, buttonWidth, buttonHeight, LEFT_ARM);
+        rightArmButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.RIGHT_ARM),0, 0, buttonWidth, buttonHeight, RIGHT_ARM);
+        torsoButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.TORSO),0, 0, buttonWidth, buttonHeight, TORSO);
+        leftLegButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.LEFT_LEG),0, 0, buttonWidth, buttonHeight, LEFT_LEG);
+        rightLegButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.RIGHT_LEG),0, 0, buttonWidth, buttonHeight, RIGHT_LEG);
+        leftFootButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.LEFT_FOOT),0, 0, buttonWidth, buttonHeight, LEFT_FOOT);
+        rightFootButton = new BodyPartHealButton(body.getPart(PlayerBodyParts.RIGHT_FOOT),0, 0, buttonWidth, buttonHeight, RIGHT_FOOT);
     }
 
     @Override
@@ -46,11 +68,12 @@ public class HealScreen extends HandledScreen<ScreenHandler> {
 
         fill(matrices, startX, startY, startX+this.backgroundWidth, startY+this.backgroundHeight, 0xff999999);
 
-        this.addDrawableChild(new ButtonWidget(centerX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, HEAD, button -> {
-            ClientNetworking.useHealingItem(PlayerBodyParts.HEAD, player.getMainHandStack());
-            this.close();
-        }));
+        headButton.x=centerX;
+        headButton.y=buttonStartY+((heightAndMargin)*row);
+        headButton.checkAndSetActive();
+        this.addDrawableChild(headButton);
         row++;
+
         this.addDrawableChild(new ButtonWidget(centerLeftX, buttonStartY+((buttonHeight+buttonMargin)*row), buttonWidth, buttonHeight, LEFT_ARM, button -> {
             ClientNetworking.useHealingItem(PlayerBodyParts.LEFT_ARM, player.getMainHandStack());
             this.close();
@@ -83,5 +106,28 @@ public class HealScreen extends HandledScreen<ScreenHandler> {
             ClientNetworking.useHealingItem(PlayerBodyParts.RIGHT_FOOT, player.getMainHandStack());
             this.close();
         }));
+    }
+
+    class BodyPartHealButton extends ButtonWidget implements ButtonWidget.PressAction {
+        public BodyPart part;
+        public BodyPartHealButton(BodyPart part, int x, int y, int width, int height, Text message) {
+            super(x, y, width, height, message, button -> button.onPress());
+            this.part = part;
+        }
+
+        public BodyPartHealButton(BodyPart part, int x, int y, int width, int height, Text message, PressAction pressAction) {
+            super(x, y, width, height, message, pressAction);
+            this.part = part;
+        }
+
+        public void checkAndSetActive(){
+            active = part.isDamaged();
+        }
+
+        @Override
+        public void onPress(ButtonWidget button) {
+            ClientNetworking.useHealingItem(part.getIdentifier(), player.getMainHandStack());
+            HealScreen.this.close();
+        }
     }
 }
