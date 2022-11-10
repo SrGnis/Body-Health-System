5f8e393125538356ecd38598ff77736648b31998
SrGnis <gnisms97@gmail.com>
2022-11-10 15:03:26 +0100

    refactor(hud): HUD creation simplified

--- src/main/java/xyz/srgnis/bodyhealthsystem/client/hud/BHSHud.java
+++ src/main/java/xyz/srgnis/bodyhealthsystem/client/hud/BHSHud.java
@@ -4,23 +4,26 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
 import xyz.srgnis.bodyhealthsystem.body.BodyPart;
-import xyz.srgnis.bodyhealthsystem.body.player.PlayerBodyParts;
 import xyz.srgnis.bodyhealthsystem.body.player.BodyProvider;
+import xyz.srgnis.bodyhealthsystem.body.player.PlayerBodyParts;
 
 import static net.minecraft.client.gui.DrawableHelper.fill;
 
 public class BHSHud implements HudRenderCallback {
-    final int dark_green = 0xff38761d;
-    final int green = 0xff8fce00;
-    final int red = 0xffb70000;
-    final int black = 0xff191919;
-    final int yellow = 0xffffd966;
-    final int orange = 0xfff87c00;
-    final int gray = 0xff5b5b5b;
+    static final int dark_green = 0xff38761d;
+    static final int green = 0xff8fce00;
+    static final int red = 0xffb70000;
+    static final int black = 0xff191919;
+    static final int yellow = 0xffffd966;
+    static final int orange = 0xfff87c00;
+    static final int gray = 0xff5b5b5b;
 
     //TODO: Position config, select color in the parts
     @Override
     public void onHudRender(MatrixStack matrixStack, float v) {
+        int startX = 0;
+        int startY = 0;
+
         BodyProvider player = (BodyProvider)MinecraftClient.getInstance().player;
 
         if (player != null) {
@@ -28,34 +31,26 @@ public class BHSHud implements HudRenderCallback {
             matrixStack.push();
             //head
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.HEAD));
-            fill(matrixStack, 10, 0, 30, 20, black);
-            fill(matrixStack, 12, 2, 28, 18, color);
+            drawHealthRectangle(matrixStack, startX+ HUDConstants.HEAD_X_OFFSET, startY+HUDConstants.HEAD_Y_OFFSET, HUDConstants.HEAD_WIDTH, HUDConstants.HEAD_HEIGHT, color);
             //arm
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.LEFT_ARM));
-            fill(matrixStack, 0, 20, 10, 50, black);
-            fill(matrixStack, 2, 22, 8, 48, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.LEFT_ARM_X_OFFSET, startY+HUDConstants.LEFT_ARM_Y_OFFSET, HUDConstants.LEFT_ARM_WIDTH, HUDConstants.LEFT_ARM_HEIGHT, color);
             //torso
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.TORSO));
-            fill(matrixStack, 10, 20, 30, 50, black);
-            fill(matrixStack, 12, 22, 28, 48, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.TORSO_X_OFFSET, startY+HUDConstants.TORSO_Y_OFFSET, HUDConstants.TORSO_WIDTH, HUDConstants.TORSO_HEIGHT, color);
             //arm
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.RIGHT_ARM));
-            fill(matrixStack, 30, 20, 40, 50, black);
-            fill(matrixStack, 32, 22, 38, 48, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.RIGHT_ARM_X_OFFSET, startY+HUDConstants.RIGHT_ARM_Y_OFFSET, HUDConstants.RIGHT_ARM_WIDTH, HUDConstants.RIGHT_ARM_HEIGHT, color);
             //legs
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.LEFT_LEG));
-            fill(matrixStack, 10, 50, 20, 70, black);
-            fill(matrixStack, 12, 52, 18, 68, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.LEFT_LEG_X_OFFSET, startY+HUDConstants.LEFT_LEG_Y_OFFSET, HUDConstants.LEFT_LEG_WIDTH, HUDConstants.LEFT_LEG_HEIGHT, color);
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.RIGHT_LEG));
-            fill(matrixStack, 20, 50, 30, 70, black);
-            fill(matrixStack, 22, 52, 28, 68, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.RIGHT_LEG_X_OFFSET, startY+HUDConstants.RIGHT_LEG_Y_OFFSET, HUDConstants.RIGHT_LEG_WIDTH, HUDConstants.RIGHT_LEG_HEIGHT, color);
             //foot
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.LEFT_FOOT));
-            fill(matrixStack, 10, 70, 20, 80, black);
-            fill(matrixStack, 12, 72, 18, 78, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.LEFT_FOOT_X_OFFSET, startY+HUDConstants.LEFT_FOOT_Y_OFFSET, HUDConstants.LEFT_FOOT_WIDTH, HUDConstants.LEFT_FOOT_HEIGHT, color);
             color = selectHealthColor(player.getBody().getPart(PlayerBodyParts.RIGHT_FOOT));
-            fill(matrixStack, 20, 70, 30, 80, black);
-            fill(matrixStack, 22, 72, 28, 78, color);
+            drawHealthRectangle(matrixStack, startX+HUDConstants.RIGHT_FOOT_X_OFFSET, startY+HUDConstants.RIGHT_FOOT_Y_OFFSET, HUDConstants.RIGHT_FOOT_WIDTH, HUDConstants.RIGHT_FOOT_HEIGHT, color);
             matrixStack.pop();
         }
     }
@@ -78,5 +73,13 @@ public class BHSHud implements HudRenderCallback {
             return red;
         }
         return gray;
+    }
+
+    public static void drawHealthRectangle(MatrixStack matrixStack, int startX, int startY, int width, int height, int color){
+        int endX = startX+width;
+        int endY = startY+height;
+
+        fill(matrixStack, startX, startY, endX, endY, black);
+        fill(matrixStack, startX+HUDConstants.BORDER_SIZE, startY+HUDConstants.BORDER_SIZE, endX-HUDConstants.BORDER_SIZE, endY-HUDConstants.BORDER_SIZE, color);
     }
 }
